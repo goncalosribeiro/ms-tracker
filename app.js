3ba7e0507a4df28b109c9f452a9377ae05fe5dbb
Goncalo Ribeiro <goncalosribeiro@gmail.com>
2020-05-16 00:16:57 +0100

    work in progress

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render('main');
 });
 
 
-
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('server started on port 3000');
 });
